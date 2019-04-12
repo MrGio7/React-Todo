bdd9439228b0cfddef3d867ee5a226f084928cf3
MrGio7 <lashagiorgi1233@gmail.com>
2019-04-12 16:21:44 +0400

    started REact Todo project, added Todo form and list to js

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoForm = () =>{
+    return(
+        <form>
+            <input />
+        </form>
+    )
+}
+
+export default TodoForm;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,26 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+const Organize = {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+};
+
+const Bake = {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+}
+
+function TodoList(){
+    return(
+        <div className='TodoList'>
+        <Todo tasks={Organize} />
+        <Todo tasks={Bake} />
+        </div>
+    )
+}
+
+
+export default TodoList;
